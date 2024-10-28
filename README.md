96f344e97b764f397a97d60701ca38c45ed9acf2
shiva-kakanavaram <shiva.kakanavaram@gmail.com>
2024-10-28 14:14:37 +0530

    Adding Project files

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# VIT Bhopal Examination Portal
+
+Welcome to the VIT Bhopal Examination Portal! This platform is designed to streamline the examination process for students and faculty at VIT Bhopal.
+
+![VIT Bhopal Examination Portal](https://github.com/akhilthirunalveli/VIT-Examination-portal/blob/main/VIT-Exam%20Page.png)
+
+## Table of Contents
+
+- [Features](#features)
+- [Demo](#demo)
+- [Technologies Used](#technologies-used)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+
+
+## Features
+
+- **User Authentication:** Secure login and registration for students and faculty.
+- **Exam Management:** Create, schedule, and manage exams with ease.
+- **Student Dashboard:** View upcoming exams, exam history, and results.
+- **Admin Dashboard:** Manage users, exams, and results.
+- **Notifications:** Receive updates about upcoming exams and results.
+
+## Demo
+
+Check out the live demo of the portal [here](https://examination-portal-nine.vercel.app/).
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- SQL
+- SENDER
+
+## Getting Started
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+Make sure you have a modern web browser installed.
+
+### Installation
+
+1. **Clone the repo**
+
+   ```sh
+   git clone https://github.com/your-username/vit-bhopal-examination-portal.git
+   cd vit-bhopal-examination-portal
+   ```
+
+2. **Open the project**
+
+   Open the `index.html` file in your preferred web browser.
+
+
+## Usage
+
+1. **Register as a new user** or **log in** if you already have an account.
+2. **Admin users** can create and manage exams from the admin dashboard.
+3. **Students** can view their upcoming exams, take exams, and check results from their dashboard.
+
---
+++ timeTable.js
@@ -0,0 +1,139 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '9:00 AM - 10:00 AM',
+        roomNumber: '304CM',
+        subject: 'DBMS',
+        type: 'Theory'
+    },
+    {   
+        time: '10:00 AM - 11:00 AM',
+        roomNumber: '304CM',
+        subject: 'ADSJ',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11:10 AM - 1:10 PM',
+        roomNumber: '304CM',
+        subject: 'DWDM',
+        type: 'Theory'
+    },
+    {   
+        time: '2:00 PM - 3:40 PM',
+        roomNumber: '110 Old cellar lab',
+        subject: 'DWDM',
+        type: 'Lab'
+    }
+]
+const Tuesday =[
+    {   
+        time: '9:00 AM',
+        roomNumber: '27-304Y',
+        subject: 'MTH166',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'CS200',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'CS849',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'CS849',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Lecture'
+    }
+]
